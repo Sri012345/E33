02abf33e5ee86658ddd0b582c9b9cd767d92c84c
SRI DEVI <SRI DEVI@LAPTOP-O554VULI>
2025-11-22 17:01:45 +0530

    New Commit Project

---
+++ E33/src/test/java/Demo/Dummy.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Students");
+		System.out.println("Hello Students");
+		System.out.println("Hello Students");
+		System.out.println("Hello Students");
+		System.out.println("Hello Students");
+
+	}
+
+}
